3db1b92c975dd5477c095b877765dfb0fcde824f
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2023-01-10 14:39:33 +0200

    fix: 9.2: Add argument parser to exerciseCalculator

--- src/exerciseCalculator.ts
+++ src/exerciseCalculator.ts
@@ -2,26 +2,50 @@ interface Result {
   periodLength: number;
   trainingDays: number;
   success: boolean;
-  rating?: number;
-  ratingDescription?: string;
-  target?: number;
-  average?: number;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
-const calculateExercises = (trainingDetails:Array<number>, target:number): Result => {
-  let trainingDays:number = trainingDetails.filter(dailyHours => dailyHours > 0).length;
-  let average:number = trainingDetails.reduce((p, c) => p + c / trainingDetails.length, 0);
+interface TrainingData {
+  trainingDetails: Array<number>;
+  target: number;
+}
+
+const calculateExercises = (argv: Array<string>): Result => {
+  const { trainingDetails, target } = parseArgs(argv);
+  const trainingDays: number = trainingDetails.filter(dailyHours => dailyHours > 0).length;
+  const average: number = trainingDetails.reduce((p, c) => p + c / trainingDetails.length, 0);
 
   return {
     periodLength: trainingDetails.length,
-    trainingDays: trainingDays,
+    trainingDays,
     success: average > target ? true : false,
     rating: average >= target ? 3 : average >= target / 2 ? 2 : 1,
     ratingDescription: average >= target ? "Well done!" : average >= target / 2 ? "Not too bad but could be better" : "Goal was not met",
-    target: target,
-    average: average
-  }
-}
+    target,
+    average
+  };
+};
 
-const [first, second, third, ...rest] = process.argv;
-console.log(calculateExercises(rest.map(n => Number(n)), Number(third)));
+const parseArgs = (input: Array<string>): TrainingData => {
+  input = input.splice(2);
+  if (isNaN(Number(input[0]))) throw new Error("Value for target is not a number");
+  const target = Number(input.shift());
+  const trainingDetails: Array<number> = [];
+  input.forEach(n => {
+    if (isNaN(Number(n))) throw new Error("Not all training values are numbers");
+    trainingDetails.push(Number(n));
+  });
+  return {
+    trainingDetails,
+    target
+  };
+};
+
+if (process.argv.length > 4) {
+  console.log(calculateExercises(process.argv));
+} else {
+  throw new Error("Too few arguments");
+}
