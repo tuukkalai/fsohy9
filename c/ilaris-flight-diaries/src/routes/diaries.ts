2405b9b913ba7b51c13633f6409715ea42ad06a5
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2023-01-14 21:49:05 +0200

    Learning with Ilaris Flight Diaries, yet again

--- c/ilaris-flight-diaries/src/routes/diaries.ts
+++ c/ilaris-flight-diaries/src/routes/diaries.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import diaryServices from '../services/diaryServices';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
 
@@ -7,8 +8,28 @@ router.get('/', (_req, res) => {
   res.send(diaryServices.getNonSensitiveEntries());
 });
 
-router.post('/', (_req, res) => {
-  res.send('saving a diary');
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+    const addedEntry = diaryServices.addEntry(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong: ';
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
--- c/ilaris-flight-diaries/src/services/diaryServices.ts
+++ c/ilaris-flight-diaries/src/services/diaryServices.ts
@@ -1,5 +1,9 @@
 import diaryData from '../../data/diaries.json';
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import {
+  DiaryEntry,
+  NonSensitiveDiaryEntry,
+  NewDiaryEntry
+} from '../types';
 
 const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
 
@@ -16,12 +20,23 @@ const getNonSensitiveEntries = (): Array<NonSensitiveDiaryEntry> => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const addEntry = (newEntry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...newEntry
+  };
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
 };
 
 export default {
   getEntries,
   getNonSensitiveEntries,
-  addDiary
+  addEntry,
+  findById
 };
