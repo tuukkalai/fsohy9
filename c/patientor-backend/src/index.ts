0528fb3385ab32be0561730b418737fed731e119
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2023-01-14 12:39:13 +0200

    9.10: Add diagnoses, restructure project folder

--- c/patientor-backend/src/index.ts
+++ c/patientor-backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnoseRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
@@ -6,6 +7,8 @@ app.use(express.json());
 app.get('/api/ping', (_req, res) => {
   res.send('Pong');
 });
+
+app.use('/api/diagnoses/', diagnoseRouter);
 
 const PORT = 3003;
 
