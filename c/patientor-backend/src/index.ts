d078298d75326ace4e98c340143905d8fdac5402
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2023-01-15 19:00:09 +0200

    Refactor add cors to patientor backend middleware

--- c/patientor-backend/src/index.ts
+++ c/patientor-backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import diagnoseRouter from './routes/diagnoses';
 import patientRouter from './routes/patients';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('Pong');
