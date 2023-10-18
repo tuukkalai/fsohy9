733cf21dc4f2be8d6d0a5e483dc8f71bc1f4fca3
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2023-10-18 21:46:56 +0300

    9.18: Done

--- d/flight-diary-frontend/src/App.tsx
+++ d/flight-diary-frontend/src/App.tsx
@@ -1,56 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
-import { DiaryEntry, NonSensitiveDiaryEntry } from './types';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from "./types";
 
 const App = () => {
   const [entries, setEntries] = useState<NonSensitiveDiaryEntry[]>([]);
   const [newEntry, setNewEntry] = useState({
-    date: '',
-    weather: 'sunny',
-    visibility: 'ok',
-    comment: ''
+    date: "",
+    weather: "",
+    visibility: "",
+    comment: "",
   });
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries').then(response => {
-      setEntries(response.data);
-    })
+    axios.get<DiaryEntry[]>("http://localhost:3000/api/diaries").then((res) => {
+      setEntries(res.data);
+    });
   }, []);
 
   const createEntry = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    axios.post<NonSensitiveDiaryEntry>('http://localhost:3000/api/diaries', newEntry).then(response => {
-      setEntries(entries.concat(response.data));
-    })
-  }
+    axios
+      .post<NonSensitiveDiaryEntry>("http://localhost:3000/api/diaries", newEntry)
+      .then((res) => {
+        setEntries(entries.concat(res.data));
+      })
+      .catch((error) => {
+        setErrorMessage(error.response.data);
+      });
+  };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
     setNewEntry({ ...newEntry, [name]: value });
-  }
+  };
 
   return (
     <div>
       <h1>Ilari's fligh diary application</h1>
       <h2>Add new entry</h2>
+      {errorMessage && <div style={{ color: "#e22" }}>{errorMessage}</div>}
       <form onSubmit={createEntry}>
         <label htmlFor="date">Date</label>
-        <input type="text" name="date" value={newEntry.date} onChange={handleInputChange} />
+        <input
+          type="text"
+          name="date"
+          value={newEntry.date}
+          onChange={handleInputChange}
+        />
         <br />
         <label htmlFor="visibility">Visibility</label>
-        <input type="text" name="visibility" value={newEntry.visibility} onChange={handleInputChange} />
+        <input
+          type="text"
+          name="visibility"
+          value={newEntry.visibility}
+          onChange={handleInputChange}
+        />
         <br />
         <label htmlFor="weather">Weather</label>
-        <input type="text" name="weather" value={newEntry.weather} onChange={handleInputChange} />
+        <input
+          type="text"
+          name="weather"
+          value={newEntry.weather}
+          onChange={handleInputChange}
+        />
         <br />
         <label htmlFor="comment">Comment</label>
-        <input type="text" name="comment" value={newEntry.comment} onChange={handleInputChange} />
+        <input
+          type="text"
+          name="comment"
+          value={newEntry.comment}
+          onChange={handleInputChange}
+        />
         <br />
         <input type="submit" />
       </form>
       <h2>Entries</h2>
-      {entries.map(e => 
+      {entries.map((e) => (
         <div key={e.id}>
           <h3>{e.date}</h3>
           <ul>
@@ -58,9 +85,9 @@ const App = () => {
             <li>Weather: {e.weather}</li>
           </ul>
         </div>
-      )}
+      ))}
     </div>
   );
-}
+};
 
 export default App;
